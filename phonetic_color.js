735c129aac8689d01503c2246339dd7769b1570a
madwed <i.madwed@gmail.com>
2015-04-04 10:41:51 -0400

    Planning Stage
    Added plan

---
+++ phonetic_color.js
@@ -0,0 +1,12 @@
+/* 
+Input string
+Convert to phonemes
+	Options
+		Use dictionary server
+		Use local dictionary
+			Access local dictionary?
+			Separate dictionary file
+Translate phonemes to color
+	Create consonant map
+	Decide default values for vowel sounds
+*/
