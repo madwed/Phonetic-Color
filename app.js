53e001c055c1ed83fef7c4bdb7ce5b94565c1fcd
Isaac Madwed <i.madwed@gmail.com>
2015-07-07 23:22:49 -0400

    removed morgan from app.js

--- app.js
+++ app.js
@@ -5,12 +5,9 @@
 var path = require("path"),
 	express = require("express"),
 	routes = require(path.join(__dirname, "router.js")),
-	logger = require("morgan"),
 	bodyParser = require("body-parser"),
 	app = express();
 
-
-app.use(logger("dev"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.text());
 app.use("/", routes);
