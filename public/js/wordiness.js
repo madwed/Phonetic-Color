adc45975727a7dc2bbe917b65fc8a73e8de46165
Isaac Madwed <i.madwed@gmail.com>
2015-07-18 09:56:37 -0400

    comment wordiness

--- public/js/wordiness.js
+++ public/js/wordiness.js
@@ -1,5 +1,6 @@
 (function () {
 
+//Start of function to create a graph of length of word in phonemes over word
 Operator.prototype.wordy = function (string) {
 	var wordiness = string.split("J");
 	wordiness = wordiness.map(function (word) {
